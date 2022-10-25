a3ef75b211591b6d2e00dc3c506292174a82986e
Liam Hayden <l.hayden67@outlook.com>
2022-10-25 14:31:17 +0100

    Add seeds for airport codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Airport::delete_all
+
+airports = Airport.create([{airport_code: "DUB"}, {airport_code: "KER"}, {airport_code: "CRK"}])
