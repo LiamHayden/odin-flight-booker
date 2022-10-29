dab83fd21f91359c9433fd2d51456c5cf379c1b3
Liam Hayden <l.hayden67@outlook.com>
2022-10-29 16:54:58 +0100

    Add search form and fix flight seeds

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @airport_options = Airport.all.map{ |u| [ u.airport_code, u.id ] }
+    @passengers = ((1 .. 4).map { |p| [p,p] } )
+    @dates = Flight.all.map{ |u| [ u.start_datetime, u.id ] }
   end
 end
